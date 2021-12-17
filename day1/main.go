ad153e1cdcac7d5454ec955019fc4640415cba97
bureaucratic2 <283264743@qq.com>
2021-12-17 18:56:25 +0800

    finish day1

--- day1/main.go
+++ day1/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	s := common.Load()
-	fmt.Println("part1: ", part1(s))
+	fmt.Println("part1:", part1(s))
+	fmt.Println("part2:", part2(s))
 }
 
 func part1(s string) int {
@@ -24,6 +25,38 @@ func part1(s string) int {
 	for scanner.Scan() {
 		next, err := strconv.Atoi(scanner.Text())
 		common.Check(err)
+		if next > prev {
+			count++
+		}
+		prev = next
+	}
+
+	return count
+}
+
+func part2(s string) int {
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	vec := make([]int, 0)
+
+	for scanner.Scan() {
+		next, err := strconv.Atoi(scanner.Text())
+		common.Check(err)
+		vec = append(vec, next)
+	}
+
+	if len(vec) <= 3 {
+		return 0
+	}
+
+	var prev int
+	for _, n := range vec[:3] {
+		prev += n
+	}
+
+	var next int
+	var count int
+	for idx, n := range vec[3:] {
+		next = prev - vec[idx] + n
 		if next > prev {
 			count++
 		}
